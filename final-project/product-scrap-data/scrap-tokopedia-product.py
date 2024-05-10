8695a91f528c169a2fcecd582d02f173054e7e7c
Picessa Kresna <kresna.picessa@gmail.com>
2024-05-10 21:39:57 +0800

    add scrape file

---
+++ final-project/product-scrap-data/scrap-tokopedia-product.py
@@ -0,0 +1,179 @@
+import csv
+import os
+from collections import defaultdict
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
+from selenium.webdriver.common.alert import Alert
+from time import sleep
+
+driver = webdriver.Chrome()
+scrapecounter = 0  # Initialize scrape counter
+
+def loaded_page(self, element):
+    global myElem
+    delay = 5
+    try:
+        myElem = WebDriverWait(self, delay).until(EC.presence_of_element_located((By.XPATH, element)))
+    except TimeoutException:
+        print('Loading too much time')
+
+    return myElem
+
+def save_produk_info_to_csv(product_info):
+    directory = 'product-scrap-data/data-produk'
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    # Define file path
+    file_path = os.path.join(directory, 'product-goodgamingshop.csv')
+
+    # Check if file exists, if not, create and write header
+    if not os.path.exists(file_path):
+        with open(file_path, 'w', newline='', encoding='utf-8') as csvfile:
+            fieldnames = ['Nama Produk', 'Kategori', 'Jumlah', 'Harga Jual', 'Harga Awal', 'Diskon', 'Deskripsi', 'Rating', 'Rating Counter', 'Link']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+
+    # Append product info to CSV file
+    with open(file_path, 'a', newline='', encoding='utf-8') as csvfile:
+        fieldnames = ['Nama Produk', 'Kategori', 'Jumlah', 'Harga Jual', 'Harga Awal', 'Diskon', 'Deskripsi', 'Rating', 'Rating Counter', 'Link']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        for info in product_info:
+            writer.writerow(info)
+
+
+def get_produkinfo():
+    global scrapecounter  # Access the global scrape counter variable
+
+    nama_produk = loaded_page(driver, '//*[@id="pdp_comp-product_content"]/div/h1').text
+    scrapecounter += 1  # Increment scrape counter
+
+    kategori1_produk = loaded_page(driver, '//*[@id="main-pdp-container"]/div[1]/nav/ol/li[1]').text
+    kategori2_produk = loaded_page(driver, '//*[@id="main-pdp-container"]/div[1]/nav/ol/li[2]').text
+    kategori3_produk = loaded_page(driver, '//*[@id="main-pdp-container"]/div[1]/nav/ol/li[3]').text
+    kategori4_produk = loaded_page(driver, '//*[@id="main-pdp-container"]/div[1]/nav/ol/li[4]').text
+    kategori_produk = '|'.join([kategori1_produk, kategori2_produk, kategori3_produk, kategori4_produk])
+
+    try:
+        jumlah_produk_element = driver.find_element(By.XPATH,'//*[@id="pdp_comp-product_content"]/div/div[1]/div/p[1]')
+        jumlah_produk = loaded_page(driver, '//*[@id="pdp_comp-product_content"]/div/div[1]/div/p[1]').text
+    except NoSuchElementException:
+        try:
+            jumlah_produk_element = driver.find_element(By.XPATH, '//*[@id="pdp_comp-product_content"]/div/div[1]/div/p')
+            jumlah_produk = jumlah_produk_element.text
+        except NoSuchElementException:
+            jumlah_produk = 0
+
+    try:
+        hargajual_produk_element = driver.find_element(By.XPATH,'//*[@id="pdp_comp-product_content"]/div/div[2]/div[2]')
+        hargajual_produk = loaded_page(driver, '//*[@id="pdp_comp-product_content"]/div/div[2]/div[1]').text
+    except NoSuchElementException:
+        hargajual_produk = loaded_page(driver, '//*[@id="pdp_comp-product_content"]/div/div[2]/div').text
+
+    try:
+        hargaawal_produk_element = driver.find_element(By.XPATH,'//*[@id="pdp_comp-product_content"]/div/div[2]/div[2]')
+        hargaawal_produk = loaded_page(driver, '//*[@id="pdp_comp-product_content"]/div/div[2]/div[2]/div[2]/span[2]').text
+    except NoSuchElementException:
+        try:
+            hargaawal_produk_element = driver.find_element(By.XPATH, '//*[@id="pdp_comp-product_content"]/div/div[1]/div/p')
+            hargaawal_produk = hargajual_produk = loaded_page(driver, '//*[@id="pdp_comp-product_content"]/div/div[2]/div[1]').text
+        except NoSuchElementException:
+            hargaawal_produk = hargajual_produk = loaded_page(driver, '//*[@id="pdp_comp-product_content"]/div/div[2]/div').text
+
+    try:
+        diskon_produk_element = driver.find_element(By.XPATH,'//*[@id="pdp_comp-product_content"]/div/div[2]/div[2]')
+        diskon_produk = loaded_page(driver, '//*[@id="pdp_comp-product_content"]/div/div[2]/div[2]/div[1]/span[2]').text
+    except NoSuchElementException:
+        try:
+            diskon_produk_element = driver.find_element(By.XPATH, '//*[@id="pdp_comp-product_content"]/div/div[1]/div/p')
+            diskon_produk = 0
+        except NoSuchElementException:
+            diskon_produk = 0
+
+    deskripsi_produk = loaded_page(driver, '//*[@id="pdp_comp-product_detail"]/div[2]/div[2]/div/span/span/div').text
+
+    try:
+        rating_produk_element = driver.find_element(By.XPATH,'//*[@id="pdp_comp-product_content"]/div/div[1]/div/p[1]')
+        rating_produk = loaded_page(driver, '//*[@id="pdp_comp-product_content"]/div/div[1]/div/p[2]/span[1]/span[2]').text
+    except NoSuchElementException:
+        try:
+            rating_produk_element = driver.find_element(By.XPATH, '//*[@id="pdp_comp-product_content"]/div/div[1]/div/p')
+            rating_produk = 0
+        except NoSuchElementException:
+            try:
+                rating_produk_element = driver.find_element(By.XPATH, '//*[@id="pdp_comp-product_content"]/div/div[1]/div/p[2]')
+                rating_produk = 0
+            except NoSuchElementException:
+                rating_produk = 0
+
+    try:
+        ratingcounter_produk_element = driver.find_element(By.XPATH,'//*[@id="pdp_comp-product_content"]/div/div[1]/div/p[1]')
+        ratingcounter_produk = loaded_page(driver, '//*[@id="pdp_comp-product_content"]/div/div[1]/div/p[2]/span[2]').text
+    except NoSuchElementException:
+        try:
+            ratingcounter_produk_element = driver.find_element(By.XPATH, '//*[@id="pdp_comp-product_content"]/div/div[1]/div/p')
+            ratingcounter_produk = 0
+        except NoSuchElementException:
+            ratingcounter_produk = 0
+
+    link_produk = driver.current_url
+
+    # Append product info to CSV
+    product_info = [{
+        'Nama Produk': nama_produk,
+        'Kategori': kategori_produk,
+        'Jumlah': jumlah_produk,
+        'Harga Jual': hargajual_produk,
+        'Harga Awal': hargaawal_produk,
+        'Diskon': diskon_produk,
+        'Deskripsi': deskripsi_produk,
+        'Rating': rating_produk,
+        'Rating Counter': ratingcounter_produk,
+        'Link': link_produk
+    }]
+
+    save_produk_info_to_csv(product_info)
+    kata = nama_produk.split()
+    info_nama = kata[:3]
+    print_produk = ' '.join(info_nama)
+    print(f'Jumlah Produk Terscrape = {scrapecounter}, add {print_produk} reviews to CSV')
+
+def get_allproduct():
+    for i in range(1, 80):
+        try:
+            alamatbarang = loaded_page(driver, f'//*[@id="zeus-root"]/div/div[2]/div[2]/div[4]/div/div[2]/div[1]/div[{i}]/div/div/div/div/div/div[1]/a').get_attribute('href')
+            driver.get(alamatbarang)
+            sleep(1)
+
+            get_produkinfo()
+
+            driver.back()
+            sleep(1)
+        except NoSuchElementException:
+            print('No more product')
+            return False
+    return True
+
+def get_merchantinfo():
+    i = 1
+    while True:
+        try:
+            driver.get(f"https://www.tokopedia.com/goodgamingshop/product/page/{i}?sort=8")
+            sleep(2)
+            if not get_allproduct():
+                print('Scrapping selesai')
+                break
+            i += 1
+        except TimeoutException:
+            print('Loading too much time')
+            break
+
+def main():
+    get_merchantinfo()
+    
+if __name__ == "__main__":
+    main()
---
+++ final-project/product-scrap-data/scrap-tokopedia-review.py
@@ -0,0 +1,172 @@
+import csv
+import os
+import pandas as pd
+from collections import defaultdict
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
+from selenium.webdriver.common.alert import Alert
+from time import sleep
+
+driver = webdriver.Chrome()
+
+def loaded_page(self, element):
+    global myElem
+    delay = 5
+    try:
+        myElem = WebDriverWait(self, delay).until(EC.presence_of_element_located((By.XPATH, element)))
+    except TimeoutException:
+        print('Loading too much time')
+
+    return myElem
+
+def save_reviewed_product_to_csv(directory, nama_produk):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    
+    file_path = os.path.join(directory, 'product-reviewed.csv')
+    
+    if not os.path.exists(file_path):
+        with open(file_path, 'w', newline='', encoding='utf-8') as csvfile:
+            fieldnames = ['Nama Produk']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+    
+    with open(file_path, 'a', newline='', encoding='utf-8') as csvfile:
+        fieldnames = ['Nama Produk']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writerow({'Nama Produk': nama_produk})
+
+def save_reviews_to_csv(directory, nama_produk_judul, nama_produk, reviews, file_number):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    
+    file_path = os.path.join(directory, f'{file_number}_{nama_produk_judul}_reviews.csv')
+    if os.path.exists(file_path):
+        print(f"Reviews file for '{nama_produk_judul}' already exists. Skipping saving.")
+        return
+    
+    with open(file_path, 'w', newline='', encoding='utf-8') as csvfile:
+        fieldnames = ['Nama Produk', 'Review', 'Nama Akun', 'Rating User', 'Ulasan Produk', 'Waktu Review', 'Varian Produk']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for review in reviews:
+            review['Nama Produk'] = nama_produk
+            writer.writerow(review)
+
+def get_ulasan():
+    i = 1
+    review_count = 0
+    count_review_element = driver.find_element(By.XPATH, '//*[@data-testid="reviewSortingSubtitle"]')
+    count_review_text = count_review_element.text
+    count = int(count_review_text.split(" ")[-2])
+    review_data = []
+
+    while i <= 50:
+        try:
+            nama_akun = loaded_page(driver, f'//*[@id="review-feed"]/article[{i}]/div/div[2]/span').text
+
+            rating_user_element = driver.find_element(By.XPATH, f'//*[@id="review-feed"]/article[{i}]/div/div[1]/div/div')
+            rating_user = rating_user_element.get_attribute('aria-label')
+
+            waktu_review = loaded_page(driver, f'//*[@id="review-feed"]/article[{i}]/div/div[1]/div/p').text
+
+            xpath_article = f'//*[@id="review-feed"]/article[{i}]'
+
+            try:
+                varian_element = driver.find_element(By.XPATH, xpath_article + '//*[@data-testid="lblVarian"]')
+                varian_product = varian_element.text
+            except NoSuchElementException:
+                varian_product = "none"
+
+            try:
+                ulasan_produk_element = driver.find_element(By.XPATH, f'{xpath_article}//*[@data-testid="lblItemUlasan"]')
+                ulasan_produk = ulasan_produk_element.text
+            except NoSuchElementException:
+                ulasan_produk = "none"
+
+        except NoSuchElementException:
+            pass
+
+        review_count += 1
+        print(f'Review {review_count}: {nama_akun}, {rating_user}, {ulasan_produk}')
+        
+        review_data.append({
+            'Review': review_count,
+            'Nama Akun': nama_akun,
+            'Rating User': rating_user,
+            'Ulasan Produk': ulasan_produk,
+            'Waktu Review': waktu_review,
+            'Varian Produk': varian_product
+        })
+        
+        if review_count == count:
+            break
+
+        i += 1
+        
+        if i > 50:
+            button_next = loaded_page(driver, f'//*[@id="zeus-root"]/div/main/div[2]/div[1]/div[2]/section/div[3]/nav/ul/li[11]/button')
+            button_next.click()
+            sleep(1)
+            i = 1
+
+    return review_data
+
+def load_ulasan(link):
+    driver.get(link)
+    sleep(1)
+    try:
+        # Scroll to the review section
+        review_section = driver.find_element(By.XPATH, '//*[@id="pdp_comp-product_detail_media"]')
+        driver.execute_script("arguments[0].scrollIntoView();", review_section)
+        sleep(1)
+        
+        # Check if review section is empty
+        try:
+            empty_state_element = driver.find_element(By.XPATH, '//*[@id="review-feed"]/div/div[@data-unify="EmptyState"]')
+            print("No reviews found.")
+            return None
+        except NoSuchElementException:
+            pass
+
+        # Click the "View All Feedback" button
+        all_review_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@data-testid="btnViewAllFeedback"]')))
+        href_value = all_review_button.get_attribute("href")
+        driver.get(href_value)
+        sleep(1)
+
+        # Get reviews
+        reviews = get_ulasan()
+    except NoSuchElementException:
+        reviews = None
+
+    return reviews
+
+def get_link():
+    df = pd.read_csv('product-scrap-data/data-produk/product-goodgamingshop.csv')
+
+    file_number = 741  # Initialize file number
+
+    for index, row in df.iloc[2471:].iterrows():
+        nama_produk_judul = ' '.join(row['Nama Produk'].split()[:1])  # Limit to first four words
+        nama_produk = row['Nama Produk']
+        link = row['Link']
+        
+        reviews = load_ulasan(link)
+        
+        if reviews is not None:
+            save_reviews_to_csv('product-scrap-data/data-ulasan', nama_produk_judul, nama_produk, reviews, file_number)
+            save_reviewed_product_to_csv('product-scrap-data/data-produk', nama_produk)
+            file_number += 1  # Increment file number for the next iteration
+        else:
+            print(f"No reviews found for '{nama_produk_judul}'. CSV file not saved.")
+
+def main():
+    get_link()
+    
+if __name__ == "__main__":
+    main()
